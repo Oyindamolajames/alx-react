d9b4f5321f17fc72c3a07d00a91b7b6f6094c8a6
Ogunsanya James <oyindamolajames@gmail.com>
2024-04-03 18:09:49 +0100

    added test

--- 0x01-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x01-react_intro/task_3/dashboard/src/utils.test.js
@@ -1,14 +1,14 @@
 import { getFullYear, getFooterCopy, getLatestNotification } from "./utils";
 
-test("test correct current year", () => {
+test("correct current year", () => {
   expect(getFullYear()).toBe(2022);
 });
 
-test("test correct footer copy", () => {
+test("correct footer copy", () => {
   expect(getFooterCopy(true)).toBe("Holberton School");
   expect(getFooterCopy(false)).toBe("Holberton School main dashboard");
 });
 
-test("test correct right notification", () => {
+test("correct right notification", () => {
   expect(getLatestNotification()).toBe("<strong>Urgent requirement</strong> - complete by EOD");
 });
